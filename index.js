299cf8c5d706099d4e80c0a36bb62e5bc87fdf49
Gaurav Nelson <gnelson@redhat.com>
2017-06-23 14:59:46 +1000

    Added command line switches to show progress

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var _ = require('lodash');
 var async = require('async');
 var linkCheck = require('link-check');
 var asciidocLinkExtractor = require('asciidoc-link-extractor');
+var ProgressBar = require('progress');
+var bar;
+var showProgressBar = false;
+
+if (_.includes(process.argv, "--progress") || _.includes(process.argv, "-p")) {
+    showProgressBar = true;
+}
 
 module.exports = function asciidocLinkCheck(asciidoc, opts, callback) {
     if (arguments.length === 2 && typeof opts === 'function') {
@@ -12,7 +19,20 @@ module.exports = function asciidocLinkCheck(asciidoc, opts, callback) {
         opts = {};
     }
 
-    async.mapLimit(_.uniq(asciidocLinkExtractor(asciidoc)), 2, function (link, callback) {
-        linkCheck(link, opts, callback);
+    var linksCollection = _.uniq(asciidocLinkExtractor(asciidoc))
+    if (showProgressBar) {
+        bar = bar || new ProgressBar('Checking... [:bar] :percent', {
+            complete: '=',
+            incomplete: ' ',
+            width: 25,
+            total: linksCollection.length
+        });
+    }
+
+    async.mapLimit(linksCollection, 2, function (link, callback) {
+        linkCheck(link, opts, callback)
+        if (showProgressBar) {
+            bar.tick();
+        }
     }, callback);
 };
